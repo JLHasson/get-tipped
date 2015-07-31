7ae8ced4182286a20b929d6f38c6af7edb06adb2
Lance Hasson <lancehasson@gmail.com>
2015-07-31 02:56:06 +0000

    Added model

---
+++ app/models/bear.js
@@ -0,0 +1,10 @@
+//Create a bear schema
+
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var BearSchema = new Schema({
+    name: String
+});
+
+module.exports = mongoose.model('Bear', BearSchema);
