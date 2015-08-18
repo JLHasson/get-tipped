804b435d603cb791096350fa86a4d572cecf4c68
Lance Hasson <lance.hasson@gmail.com>
2015-08-18 11:44:57 -0400

    Fixing this so that whats on the repo will actually run

---
+++ public/js/controllers/SignupCtrl.js
@@ -0,0 +1,9 @@
+angular.module('SignupCtrl', [])
+    .controller('SignupController', ['$scope', '$window', '$rootScope', '$auth', function($scope, $window, $rootScope, $auth) {
+
+        // $scope.isAuthenticated = function() {
+        //     //Check if logged in
+        //     return $auth.isAuthenticated();
+        // };
+
+}]);
